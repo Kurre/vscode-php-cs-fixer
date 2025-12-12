1ed8b9db1fd478b985270e2a00772d9147172640
Tommi Finnilä <tommi.finnila@alfame.com>
2025-12-12 14:03:00 +0200

    chore: add test coverage script and update Vitest configuration for improved testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
+		environment: 'node',
 		exclude: [...configDefaults.exclude],
+		setupFiles: ['tests/setup/vitest.setup.ts'],
+		include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
 	},
 })
